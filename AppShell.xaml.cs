a14618f7bf4f48a9c8db8f63240735d66e3ba0d2
Nam-Tran-0906 <htran37593@ucumberlands.edu>
2026-02-18 23:30:58 -0500

    Initial MAUI project setup

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LocationHeatmapApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace LocationHeatmapApp;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
